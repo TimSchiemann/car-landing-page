8b71a2ad1132a0259b8cb92a516df45534cd9642
Tim Schiemann <tschiemann@unity-mail.de>
2022-07-31 16:00:18 +0200

    Add custom descriptions

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ function cycleContent(){
     // let iframes = [...document.querySelectorAll('.content__video')]
     let navItems = [...document.querySelectorAll('.content__nav-item')]
     let currentItem = 0
+    console.log(contentItems)
 
     /* DISPLAY DIFFERENT CONTENT */
     function swapContent(){
         currentItem < contentItems.length - 1? currentItem++ : currentItem = 0
         contentItems[currentItem].classList.remove('hidden')
         navItems[currentItem].classList.add('highlight')
+
         for(let i=0; i<contentItems.length; i++){
             if(i !== currentItem){
                 contentItems[i].classList.add('hidden')
@@ -18,7 +20,6 @@ function cycleContent(){
     }
     setInterval(swapContent, 10000);
 }
-
 
 cycleContent()
 
